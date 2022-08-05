baeb8af5946957179b89eb41f8d9e28ab89e2278
rixiobarrios <rixiobarrios.consulado@gmail.com>
2022-08-05 09:47:09 -0500

    Updated file

--- filter.js
+++ filter.js
@@ -2,16 +2,45 @@
 
 // The filter() method creates a shallow copy of a portion of a given array, filtered down to just the elements from the given array that pass the test implemented by the provided function.
 
-const words = [
-    'spray',
-    'limit',
-    'elite',
-    'exuberant',
-    'destruction',
-    'present',
-];
+// const words = [
+//     'spray',
+//     'limit',
+//     'elite',
+//     'exuberant',
+//     'destruction',
+//     'present',
+// ];
 
-const result = words.filter((word) => word.length > 6);
+// const result = words.filter((word) => word.length > 6);
 
-console.log(result);
+// console.log(result);
 // expected output: Array ["exuberant", "destruction", "present"]
+
+// Filter by voting age:
+// function canVote(age) {
+//     return age >= 18;
+// }
+
+// function func() {
+//     var filtered = [24, 33, 16, 40].filter(canVote);
+//     return filtered;
+// }
+// console.log(func()); //=> [24, 33, 40]
+
+// Filter by positive numbers:
+// function isPositive(value) {
+//     return value > 0;
+// }
+
+// var filtered = [112, 52, 0, -1, 944].filter(isPositive);
+
+// console.log(filtered);//=> [112, 52, 944]
+
+// Filter by even numbers:
+// function isEven(value) {
+//     return value % 2 == 0;
+// }
+
+// var filtered = [11, 98, 31, 23, 944].filter(isEven);
+
+// console.log(filtered);//=> [98, 944]
---
+++ mathfloor.js
@@ -0,0 +1,15 @@
+// MATH.FLOOR:
+
+// The Math.floor() function returns the largest integer less than or equal to a given number.
+
+console.log(Math.floor(5.95));
+// expected output: 5
+
+console.log(Math.floor(5.05));
+// expected output: 5
+
+console.log(Math.floor(5));
+// expected output: 5
+
+console.log(Math.floor(-5.05));
+// expected output: -6
